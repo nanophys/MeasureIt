48ff8534560450c1b3be2485cb95af888c6d688c
Elliott Runburg <erunburg@gmail.com>
2019-02-06 10:51:43 -0800

    USE THIS DRIVER

--- daq_driver.py
+++ daq_driver.py
@@ -1,14 +1,8 @@
 # daq_driver.py
 
-import numpy as np
-import time
-import sys
-import signal
-import collections
-
-import nidaqmx
+import nidaqmx, time
 import qcodes as qc
-from qcodes import (Instrument, ManualParameter, MultiParameter, validators as vals)
+from qcodes import (Instrument, validators as vals)
 from qcodes.instrument.channel import InstrumentChannel
 
 class Daq(Instrument):
@@ -18,6 +12,10 @@ class Daq(Instrument):
         system = nidaqmx.system.System.local()
         self.name=address
         self.device = system.devices[self.name]
+        self.ao_num = ao_num
+        self.ai_num = ai_num
+        
+        reader_tasks = []
         
         for a in range(ao_num):
             ch_name = 'ao'+str(a)
@@ -27,6 +25,33 @@ class Daq(Instrument):
             ch_name = 'ai'+str(b)
             channel = DaqAIChannel(self, self.device, self.name, ch_name)
             self.add_submodule(ch_name, channel)
+            task=nidaqmx.Task("reading " + ch_name)
+            channel.add_self_to_task(task)
+            reader_tasks.append(task)
+            
+        
+    def get_ao_num(self):
+        return self.ao_num
+    
+    def get_ai_num(self):
+        return self.ai_num
+    
+    def update_all_inputs(self):
+        for a in range(self.ai_num):
+            ch_name = 'ai' + str(a)
+            channel = self.submodules[ch_name]
+            
+            channel.get("voltage")
+    
+    def __del__(self):
+        try:
+            for a,c in self.submodules.items():
+                if c.task is not None:
+                    c.task.close()
+        except:
+            pass
+        
+        super().__del__()
         
         
 class DaqAOChannel(InstrumentChannel):
@@ -39,12 +64,12 @@ class DaqAOChannel(InstrumentChannel):
             self.channel.ao_gain=_gain
         
     def get_voltage(self):
-        return self.voltage
+        return self._voltage
     
     def set_voltage(self, _voltage):
-        self.voltage=_voltage
         if self.task != None:
             self.task.write(_voltage)
+            self._voltage=_voltage
         
     def get_load_impedance(self):
         return self.impedance
@@ -63,7 +88,7 @@ class DaqAOChannel(InstrumentChannel):
         self.fullname=self.address+"/"+self.name
         
         self.gain=-1
-        self.voltage=0
+        self._voltage=0
         self.impedance=-1
         
         self.task=None
@@ -106,6 +131,9 @@ class DaqAOChannel(InstrumentChannel):
     def clear_task(self):
         self.task=None
         self.channel=None
+        
+    def __del__(self):
+        self.clear_task()
     
     
         
@@ -121,8 +149,8 @@ class DaqAIChannel(InstrumentChannel):
         
     def get_voltage(self):
         if self.task != None:
-            self.voltage = self.task.read()
-        return self.voltage
+            self._voltage = self.task.read()
+        return self._voltage
         
     def get_load_impedance(self):
         return self.impedance
@@ -141,7 +169,7 @@ class DaqAIChannel(InstrumentChannel):
         self.fullname=self.address+"/"+self.name
         
         self.gain=-1
-        self.voltage=0
+        self._voltage=0
         self.impedance=-1
         
         self.task=None
@@ -172,6 +200,9 @@ class DaqAIChannel(InstrumentChannel):
                            )
         
     def add_self_to_task(self, task):
+        if self.task is not None:
+            return 0
+        
         task.ai_channels.add_ai_voltage_chan(self.fullname)
         self.task=task
         self.channel=nidaqmx._task_modules.channels.ai_channel.AIChannel(self.task._handle, self.channel)
@@ -179,29 +210,34 @@ class DaqAIChannel(InstrumentChannel):
             task.ai_channels.ai_gain=self.gain
         if self.impedance != -1:
             task.ai_load_impedance=self.impedance
-            
+        
+        return 1
+    
     def clear_task(self):
         self.task=None
         self.channel=None
         
+    def __del__(self):
+        self.clear_task()
         
         
 def main():
     daq=Daq("Dev1","test",2,24)
     print(daq.ai1)
     print(daq.ao1)
-    print(daq.ao1.set("gain",4))
+    daq.ao1.set("gain",4)
     print(daq.ao1.get("gain"))
     writer=nidaqmx.Task()
     reader=nidaqmx.Task()
     daq.ao1.add_self_to_task(writer)
-    daq.ai1.add_self_to_task(reader)
+    daq.ai2.add_self_to_task(reader)
     daq.ao1.set("voltage",1)
-    print(daq.ai1.get("voltage"))
-    print(daq.ai1.get("voltage"))
-    print(daq.ai1.get("voltage"))
+    print(daq.ai2.get("voltage"))
+    print(daq.ai2.get("voltage"))
+    print(daq.ai2.get("voltage"))
     writer.close()
-    print(daq.ai1.get("voltage"))
+    time.sleep(3)
+    print(daq.ai2.get("voltage"))
     reader.close()
     daq.close()
 
