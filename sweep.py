010158b90cfca64a6531280fdc1637bb4acfaf00
Elliott Runburg <erunburg@gmail.com>
2019-02-13 12:25:25 -0800

    Updated Joe's sweep class to work better within our code

--- sweep.py
+++ sweep.py
@@ -1,4 +1,3 @@
-%matplotlib qt
 import io
 import time
 import numpy as np
@@ -34,109 +33,84 @@ class Sweep(object):
         self._sr830s.append((l, name, gain))
 
     def _create_measurement(self, *set_params):
-        meas = Measurement()
+        self.meas = Measurement()
         for p in set_params:
-            meas.register_parameter(p)
-        meas.register_custom_parameter('time', label='Time', unit='s')
+            self.meas.register_parameter(p)
+        self.meas.register_custom_parameter('time', label='Time', unit='s')
         for p in self._params:
-            meas.register_parameter(p, setpoints=(*set_params, 'time',))
+            self.meas.register_parameter(p, setpoints=(*set_params, 'time',))
         for l, _, _ in self._sr830s:
-            meas.register_parameter(l.X, setpoints=(*set_params, 'time',))
-            meas.register_parameter(l.Y, setpoints=(*set_params, 'time',))
-        return meas
+            self.meas.register_parameter(l.X, setpoints=(*set_params, 'time',))
+            self.meas.register_parameter(l.Y, setpoints=(*set_params, 'time',))
+            
+        return self.meas
+        
     
-    def sweep(self, set_param, vals, inter_delay=None):
-        if inter_delay is not None:
-            d = len(vals)*inter_delay
-            h, m, s = int(d/3600), int(d/60) % 60, int(d) % 60
-            print(f'Minimum duration: {h}h {m}m {s}s')
-
-        fig = plt.figure(figsize=(4*(2 + len(self._params) + len(self._sr830s)),4))
-        grid = plt.GridSpec(4, 1 + len(self._params) + len(self._sr830s), hspace=0)
-        setax = fig.add_subplot(grid[:, 0])
-        setax.set_xlabel('Time (s)')
-        setax.set_ylabel(f'{set_param.label} ({set_param.unit})')
-        setaxline = setax.plot([], [])[0]
-
-        paxs = []
-        plines = []
+    def set_figs(self, fig, setax, axes):
+        self.fig = fig
+        self.setax = setax
+        self.axes = axes
+    
+        self.setax.set_xlabel('Time (s)')
+        self.setax.set_ylabel(f'{self.set_param.label} ({self.set_param.unit})')
+        self.setaxline = setax.plot([], [])[0]
+        
+        self.plines = []
         for i, p in enumerate(self._params):
-            ax = fig.add_subplot(grid[:, 1 + i])
-            ax.set_xlabel(f'{set_param.label} ({set_param.unit})')
-            ax.set_ylabel(f'{p.label} ({p.unit})')
-            paxs.append(ax)
-            plines.append(ax.plot([], [])[0])
+            self.axes[i].set_xlabel(f'{self.set_param.label} ({self.set_param.unit})')
+            self.axes[i].set_ylabel(f'{p.label} ({p.unit})')
 
-        laxs = []
-        llines = []
-        for i, (l, name, _) in enumerate(self._sr830s):
-            ax0 = fig.add_subplot(grid[:-1, 1 + len(self._params) + i])
-            ax0.set_ylabel(f'{name} (V)')
-            fmt = ScalarFormatter()
-            fmt.set_powerlimits((-3, 3))
-            ax0.get_yaxis().set_major_formatter(fmt)
-            laxs.append(ax0)
-            llines.append(ax0.plot([], [])[0])
-            ax1 = fig.add_subplot(grid[-1, 1 + len(self._params) + i], sharex=ax0)
-            ax1.set_ylabel('Phase (°)')
-            ax1.set_xlabel(f'{set_param.label} ({set_param.unit})')
-            laxs.append(ax1)
-            llines.append(ax1.plot([], [])[0])
-            plt.setp(ax0.get_xticklabels(), visible=False)
-
-        fig.tight_layout()
-        fig.show()
-
-        meas = self._create_measurement(set_param)
-        with meas.run() as datasaver:
-            t0 = time.monotonic()
-            for setpoint in vals:
-                t = time.monotonic() - t0
-                set_param.set(setpoint)
+            self.plines.append(self.axes[i].plot([], [])[0])
+    
+    def iterate(self, datasaver):
+        t = time.monotonic() - self.t0
+        self.setpoint += self.step
+        self.set_param.set(self.setpoint)
                 
-                setaxline.set_xdata(np.append(setaxline.get_xdata(), t))
-                setaxline.set_ydata(np.append(setaxline.get_ydata(), setpoint))
-                setax.relim()
-                setax.autoscale_view()
+        self.setaxline.set_xdata(np.append(self.setaxline.get_xdata(), t))
+        self.setaxline.set_ydata(np.append(self.setaxline.get_ydata(), self.setpoint))
+        self.setax.relim()
+        self.setax.autoscale_view()
+        
+        if self.inter_delay is not None:
+            plt.pause(self.inter_delay)
+
+        data = [
+            (self.set_param, self.setpoint),
+            ('time', t)
+        ]
+        for i, p in enumerate(self._params):
+            v = p.get()
+            data.append((p, v))
+            self.plines[i].set_xdata(np.append(self.plines[i].get_xdata(), self.setpoint))
+            self.plines[i].set_ydata(np.append(self.plines[i].get_ydata(), v))
+            self.axes[i].relim()
+            self.axes[i].autoscale_view()
+#        print(data)
+        datasaver.add_result(*data)
                 
-                if inter_delay is not None:
-                    plt.pause(inter_delay)
+        self.fig.tight_layout()
+        self.fig.canvas.draw()
+        plt.pause(0.001)
+        return data
+    
+    def init_sweep(self, set_param, start, stop, step, freq):
+        self.set_param = set_param
+        self.start = start
+        self.stop = stop
+        self.step = step
+        self.inter_delay = 1/freq
+        self.t0 = time.monotonic()
+        self.setpoint = self.start - self.step
+        
+        d = (stop-start)/step*self.inter_delay
+        h, m, s = int(d/3600), int(d/60) % 60, int(d) % 60
+        print(f'Minimum duration: {h}h {m}m {s}s')
 
-                data = [
-                    (set_param, setpoint),
-                    ('time', t)
-                ]
-                for i, p in enumerate(self._params):
-                    v = p.get()
-                    data.append((p, v))
-                    plines[i].set_xdata(np.append(plines[i].get_xdata(), setpoint))
-                    plines[i].set_ydata(np.append(plines[i].get_ydata(), v))
-                    paxs[i].relim()
-                    paxs[i].autoscale_view()
-                for i, (l, _, gain) in enumerate(self._sr830s):
-                    _autorange_srs(l, 3)
-                    x, y = l.snap('x', 'y')
-                    x, y = x / gain, y / gain
-                    data.extend([(l.X, x), (l.Y, y)])
-                    llines[i*2].set_xdata(np.append(llines[i*2].get_xdata(), setpoint))
-                    llines[i*2].set_ydata(np.append(llines[i*2].get_ydata(), x))
-                    llines[i*2+1].set_xdata(np.append(llines[i*2+1].get_xdata(), setpoint))
-                    llines[i*2+1].set_ydata(np.append(llines[i*2+1].get_ydata(), np.arctan2(y, x) * 180 / np.pi))
-                    laxs[i*2].relim()
-                    laxs[i*2].autoscale_view()
-                    laxs[i*2+1].relim()
-                    laxs[i*2+1].autoscale_view()
+        self.meas = self._create_measurement(self.set_param)
+        return self.meas
+    
+    def save(self):
+        b = io.BytesIO()
+        self.fig.savefig(b, format='png')
 
-                datasaver.add_result(*data)
-                
-                fig.tight_layout()
-                fig.canvas.draw()
-                plt.pause(0.001)
-
-            d = time.monotonic() - t0
-            h, m, s = int(d/3600), int(d/60) % 60, int(d) % 60
-            print(f'Completed in: {h}h {m}m {s}s')
-
-            b = io.BytesIO()
-            fig.savefig(b, format='png')
-            display.display(display.Image(data=b.getbuffer(), format='png'))
