8c4e69d17b471c16fb326cc939d8b94ec3a7f6ba
Elliott Runburg <erunburg@gmail.com>
2020-04-07 01:23:09 -0700

    Added some button connections

--- GUI/GUI_Measureit.py
+++ GUI/GUI_Measureit.py
@@ -4,15 +4,42 @@ from mainwindow_ui import Ui_MainWindow
 os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
 
 class UImain(QtWidgets.QMainWindow):
+    
+    SUPPORTED_INSTRUMENTS = []
+    
     def __init__(self,parent = None):
         super(UImain,self).__init__(parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         
+        self.connect_buttons()
 
-
+        self.devices = {}
+        self.all_params = {}
+        self.track_params = {}
+        
+        
+        
         self.show()
-
+    
+    
+    def connect_buttons(self):
+        self.ui.addParameterButton.clicked.connect(self.add_parameter())
+        self.ui.deleteParameterButton.clicked.connect(self.delete_parameter())
+        self.ui.startButton.clicked.connect(self.start_sweep())
+        self.ui.addDeviceAction.triggered.connect(self.add_device())
+        
+        
+    def add_parameter(self):
+        pass
+    def delete_parameter(self):
+        pass
+    def start_sweep(self):
+        pass
+    def add_device(self):
+        pass
+    
+    
 def main():
     app = QtWidgets.QApplication(sys.argv)
     app.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
