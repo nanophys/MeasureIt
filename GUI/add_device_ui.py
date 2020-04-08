b7a604a42d609de53e09bd5d64277bbf303bef2d
Elliott Runburg <erunburg@gmail.com>
2020-04-07 23:55:11 -0700

    py files for dialog ui

---
+++ GUI/add_device_ui.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'add_device.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_addDevice(object):
+    def setupUi(self, addDevice):
+        addDevice.setObjectName("addDevice")
+        addDevice.resize(241, 178)
+        self.dialogBox = QtWidgets.QDialogButtonBox(addDevice)
+        self.dialogBox.setGeometry(QtCore.QRect(40, 130, 161, 32))
+        self.dialogBox.setOrientation(QtCore.Qt.Horizontal)
+        self.dialogBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.dialogBox.setObjectName("dialogBox")
+        self.horizontalLayoutWidget = QtWidgets.QWidget(addDevice)
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(30, 20, 182, 91))
+        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.instrumentLabel = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.instrumentLabel.setObjectName("instrumentLabel")
+        self.verticalLayout.addWidget(self.instrumentLabel)
+        self.nameLabel = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.nameLabel.setObjectName("nameLabel")
+        self.verticalLayout.addWidget(self.nameLabel)
+        self.addressLabel = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.addressLabel.setObjectName("addressLabel")
+        self.verticalLayout.addWidget(self.addressLabel)
+        self.horizontalLayout.addLayout(self.verticalLayout)
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.deviceBox = QtWidgets.QComboBox(self.horizontalLayoutWidget)
+        self.deviceBox.setObjectName("deviceBox")
+        self.verticalLayout_2.addWidget(self.deviceBox)
+        self.nameEdit = QtWidgets.QLineEdit(self.horizontalLayoutWidget)
+        self.nameEdit.setObjectName("nameEdit")
+        self.verticalLayout_2.addWidget(self.nameEdit)
+        self.addressEdit = QtWidgets.QLineEdit(self.horizontalLayoutWidget)
+        self.addressEdit.setObjectName("addressEdit")
+        self.verticalLayout_2.addWidget(self.addressEdit)
+        self.horizontalLayout.addLayout(self.verticalLayout_2)
+
+        self.retranslateUi(addDevice)
+        self.dialogBox.accepted.connect(addDevice.accept)
+        self.dialogBox.rejected.connect(addDevice.reject)
+        QtCore.QMetaObject.connectSlotsByName(addDevice)
+
+    def retranslateUi(self, addDevice):
+        _translate = QtCore.QCoreApplication.translate
+        addDevice.setWindowTitle(_translate("addDevice", "Dialog"))
+        self.instrumentLabel.setText(_translate("addDevice", "Instrument"))
+        self.nameLabel.setText(_translate("addDevice", "Name"))
+        self.addressLabel.setText(_translate("addDevice", "Address"))
+
