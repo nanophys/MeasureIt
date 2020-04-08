2ee863e0a4633449594c4cc73ea0319c93c02276
Elliott Runburg <erunburg@gmail.com>
2020-04-07 23:50:06 -0700

    Compiles all GUIs now

--- GUI/Compile_UI.py
+++ GUI/Compile_UI.py
@@ -1,8 +1,9 @@
 
 from PyQt5 import uic
 import os
-ui_file = 'mainwindow.ui'
-py_file = os.path.splitext(ui_file)[0] + "_ui.py"
-fp = open(py_file, "w")
-uic.compileUi(ui_file, fp, from_imports = True)
-fp.close()
+ui_files = ['mainwindow.ui', 'add_device.ui', 'edit_parameter.ui']
+for ui_file in ui_files:
+    py_file = os.path.splitext(ui_file)[0] + "_ui.py"
+    fp = open(py_file, "w")
+    uic.compileUi(ui_file, fp, from_imports = True)
+    fp.close()
