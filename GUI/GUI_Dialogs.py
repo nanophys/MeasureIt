63672d6fb3ad35a0c271d3e8caae1d7745ff6d37
Elliott Runburg <erunburg@gmail.com>
2020-04-20 17:43:47 -0700

    Added station dialogs and updated device dialogs

--- GUI/GUI_Dialogs.py
+++ GUI/GUI_Dialogs.py
@@ -1,44 +1,79 @@
 from PyQt5 import QtWidgets, QtCore, QtGui
-from PyQt5.QtWidgets import QListWidgetItem
+from PyQt5.QtWidgets import QListWidgetItem, QFileDialog
 import sys,os
 from GUI_Measureit import *
 from edit_parameter_ui import Ui_editParameter
 from add_device_ui import Ui_addDevice
+from save_station_ui import Ui_saveStation
+from remove_device_ui import Ui_removeDevice
+
 os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
 
 # TODO: Create a dictionary with class typings as the key, and common parameters as the values
 # TODO: From main window, grab the selected parameters when 'accept' registers
 
+class SaveStationGUI(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super(SaveStationGUI,self).__init__(parent)
+        self.parent = parent
+        self.ui = Ui_saveStation()
+        self.ui.setupUi(self)
+        self.setWindowTitle("Save Current Station")
+        self.url = None
+        
+        self.ui.browseButton.clicked.connect(self.select_file)
+        
+        
+    def select_file(self):
+        #fd = QFileDialog(self, directory=os.environ['MeasureItHome']+'\\cfg\\')
+        #fd.setFileMode(QFileDialog.AnyFile)
+        #fd.setNameFilter(tr("Station Files (*.station.yaml)"))
+        #if fd.exec_():
+        #    fileNames = fd.selectedFiles()
+        fileName = QFileDialog.getSaveFileName(self, "Save Station",
+                                       os.environ['MeasureItHome']+"\\cfg\\new_station.station.yaml",
+                                       "Stations (*.station.yaml)")
+        if len(fileName[0]) > 0:
+            self.url = fileName[0]
+            if '.station' not in self.url and '.yaml' not in self.url:
+                self.url += '.station.yaml'
+            elif '.station' in self.url and '.yaml' not in self.url:
+                self.url += '.station.yaml'
+            self.ui.locationEdit.setText(self.url)
+            
+    def get_file(self):
+        return self.url
+
 class EditParameterGUI(QtWidgets.QDialog):
-    def __init__(self, parent = None):
+    def __init__(self, devices, track_params, parent = None):
         super(EditParameterGUI,self).__init__(parent)
         self.parent = parent
+        self.devices = devices
         self.ui = Ui_editParameter()
         self.ui.setupUi(self)
                 
         self.make_connections()
-        self.new_track_params = []
-        for name, dev in self.parent.devices.items():
+        self.new_track_params = {}
+        
+        for name, dev in self.devices.items():
             self.ui.deviceBox.addItem(name, dev)
-        self.load_param_lists()
-        
-        self.show()
-        
-    
-    def load_param_lists(self):
+            
         for p in self.parent.track_params:
-            listitem = QListWidgetItem(str(p)) 
+            listitem = QListWidgetItem(p.full_name) 
             listitem.setData(32, p)
             self.ui.activeParamsWidget.addItem(listitem)
-            self.new_track_params.append(p)
+            self.new_track_params[p.full_name] = p
             
         self.update_avail_device_params(0)
         
+        self.show()
+        
+        
     def update_avail_device_params(self, index):
         self.ui.allParamsWidget.clear()
-        if len(self.parent.devices) > 0:
-            for key, p in self.parent.all_params[self.ui.deviceBox.currentText()].items():
-                listitem = QListWidgetItem(str(p)) 
+        if len(self.devices) > 0:
+            for key, p in self.ui.deviceBox.currentData().parameters.items():
+                listitem = QListWidgetItem(p.full_name) 
                 listitem.setData(32, p)
                 self.ui.allParamsWidget.addItem(listitem)
             
@@ -53,18 +88,19 @@ class EditParameterGUI(QtWidgets.QDialog):
         params = self.ui.allParamsWidget.selectedItems()
         
         for p in params:
-            if p.data(32) not in self.new_track_params:
-                listitem = QListWidgetItem(str(p.data(32)))
+            name = p.data(32).full_name
+            if name not in self.new_track_params.keys():
+                listitem = QListWidgetItem(name)
                 listitem.setData(32, p.data(32))
                 self.ui.activeParamsWidget.addItem(listitem)
-                self.new_track_params.append(p.data(32))
+                self.new_track_params[name] = p.data(32)
     
     
     def del_param(self):
         params = self.ui.activeParamsWidget.selectedItems()
         for p in params:
             self.ui.activeParamsWidget.takeItem(self.ui.activeParamsWidget.row(p))
-            self.new_track_params.remove(p.data(32))
+            self.new_track_params.pop(p.name)
             
             
     def get_new_track_params(self):
@@ -79,19 +115,35 @@ class AddDeviceGUI(QtWidgets.QDialog):
         self.ui.setupUi(self)
         self.setWindowTitle("Add Device")
         
-        self.selected = {}
-        self.selected['device'] = '(none)'
-        self.ui.deviceBox.addItem('(none)')
         for name, dev in UImain.SUPPORTED_INSTRUMENTS.items():
-            self.ui.deviceBox.addItem(name)
+            self.ui.deviceBox.addItem(name, dev)
             
-        self.ui.deviceBox.activated.connect(self.select_device)
         self.show()
             
-    def select_device(self, index):
-        self.selected['device'] = self.ui.deviceBox.itemText(index)
         
     def get_selected(self):
-        self.selected['name'] = self.ui.nameEdit.text()
-        self.selected['address'] = self.ui.addressEdit.text()
-        return self.selected
+        selected = {}
+        selected['device'] = self.ui.deviceBox.currentText()
+        selected['class'] = self.ui.deviceBox.currentData()
+        selected['name'] = self.ui.nameEdit.text()
+        selected['address'] = self.ui.addressEdit.text()
+        return selected
+    
+    
+class RemoveDeviceGUI(QtWidgets.QDialog):
+    def __init__(self, devices, parent=None):
+        super(RemoveDeviceGUI,self).__init__(parent)
+        self.parent = parent
+        self.ui = Ui_removeDevice()
+        self.ui.setupUi(self)
+        self.setWindowTitle("Remove Device")
+        
+        for name, dev in devices.items():
+            self.ui.deviceBox.addItem(name, dev)
+            
+        self.show()
+
+
+
+
+
