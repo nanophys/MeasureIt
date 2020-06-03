37bac659d5044eabcc5c99ebaecf5185ea8fba76
Elliott Runburg <erunburg@gmail.com>
2020-06-03 12:45:38 -0700

    Added new function to parse device names; updated _value_parser to throw exceptions for invalid inputs; created easy save_to_csv util function for datasets.

--- src/util.py
+++ src/util.py
@@ -6,20 +6,32 @@ import time
 import os
 import string
 import qcodes as qc
+import pandas as pd
 from qcodes import initialise_or_create_database_at
 from qcodes.dataset.data_export import get_data_by_id
 
 unit_dict = {
-        'f': 10**-15,
-        'p': 10**-12,
-        'n': 10**-9,
-        'u': 10**-6,
-        'm': 10**-3,
-        'k': 10**3,
-        'M': 10**6,
-        'G': 10**9,
-        '': 10**0
-        }
+    'f': 10 ** -15,
+    'p': 10 ** -12,
+    'n': 10 ** -9,
+    'u': 10 ** -6,
+    'm': 10 ** -3,
+    'k': 10 ** 3,
+    'M': 10 ** 6,
+    'G': 10 ** 9,
+    '': 10 ** 0
+}
+
+
+def save_to_csv(ds, fn):
+    df = ds.get_data_as_pandas_dataframe()
+    export_ds = pd.DataFrame()
+    for key, value in df.items():
+        export_ds[[key]] = value[[key]]
+
+    # Choose where you want the CSV saved
+    export_ds.to_csv(fn)
+
 
 def set_magnet_ramp_ranges(magnet, ranges):
     if not isinstance(ranges, list):
@@ -27,7 +39,7 @@ def set_magnet_ramp_ranges(magnet, ranges):
              [(1, <rate>, <max applicable current>), (2, <rate>, <max applicable current>), ..., \
              (n, <rate>, <max applicable current>]")
         return
-    
+
     magnet.write('CONF:RAMP:RATE:SEG {}'.format(len(ranges)))
     time.sleep(0.5)
     for r in ranges:
@@ -38,19 +50,22 @@ def set_magnet_ramp_ranges(magnet, ranges):
             return
         magnet.write(f'CONF:RAMP:RATE:CURR {r[0]},{r[1]},{r[2]}')
         time.sleep(0.5)
-        
+
+
 def set_experiment_sample_names(sweep, exp, samp):
     if sweep.save_data is True:
         qc.new_experiment(exp, samp)
     sweep._create_measurement()
 
-def init_database(db, exp, samp, sweep = None):
+
+def init_database(db, exp, samp, sweep=None):
     initialise_or_create_database_at(os.environ['MeasureItHome'] + '\\Databases\\' + db)
     qc.new_experiment(exp, samp)
     if sweep is not None:
         sweep._create_measurement()
-    
-def export_db_to_txt(db_fn, exp_name = None, sample_name = None):
+
+
+def export_db_to_txt(db_fn, exp_name=None, sample_name=None):
     if '.db' in db_fn:
         initialise_or_create_database_at(os.environ['MeasureItHome'] + '\\Databases\\' + db_fn)
     else:
@@ -62,18 +77,18 @@ def export_db_to_txt(db_fn, exp_name = None, sample_name = None):
             newpath = os.environ['MeasureItHome'] + '\\Origin Files\\' + exp.name
             if not os.path.exists(newpath):
                 os.makedirs(newpath)
-                
+
     count = 0
     for exp in experiments:
-        print("exp name: " + exp.name) 
+        print("exp name: " + exp.name)
         if sample_name is None or exp.sample_name is sample_name:
             print("sample name: " + exp.sample_name)
             write_sample_to_txt(exp, count)
             count += 1
-            
-    
-def write_sample_to_txt(exp, count = 0): 
-    #print(exp.data_sets())
+
+
+def write_sample_to_txt(exp, count=0):
+    # print(exp.data_sets())
     for a in exp.data_sets():
         print(a.run_id)
         data = get_data_by_id(a.run_id)
@@ -86,18 +101,18 @@ def write_sample_to_txt(exp, count = 0):
             file = open(file_path, "w")
             count += 1
             num_params = len(dataset)
-    
+
             for param in dataset:
                 file.write(param['label'] + " (" + param['unit'] + ")\t")
-        
+
             file.write("\n")
-    
+
             for i in range(len(dataset[0]['data'])):
                 for param in dataset:
                     file.write(str(param['data'][i]) + "\t")
                 file.write("\n")
             file.close()
-    
+
 
 def _value_parser(value):
     """
@@ -107,26 +122,27 @@ def _value_parser(value):
     if len(str(value)) == 0:
         raise ValueError
         return
-    
+
     value = str(value).strip()
-    
+
     if value[0] == '.':
         value = '0' + value
     # regex testing stripped value as valid factor string
     # must be exactly a number followed by (space optional) single valid factor char 
     # f = femto p = pico u = micro m = milli k = kilo M = Mega G = Giga
     regex = re.compile(r"^([-]?[\d]*.?[\d]+[\s]?)([fpnumkMG]?)$")
-    
+
     parsedVal = regex.search(value)
     if not parsedVal:
         raise ValueError
         return
-    
+
     parsedNum = float(parsedVal.groups(' ')[0])
     parsedUnit = parsedVal.groups(' ')[1]
-    parsedValue = parsedNum*unit_dict[parsedUnit]
-    
+    parsedValue = parsedNum * unit_dict[parsedUnit]
+
     return parsedValue
+
 
 def _name_parser(_name):
     """
@@ -139,15 +155,15 @@ def _name_parser(_name):
     elif not name[0].isalpha():
         raise ValueError(f'Invalid name: {name}. First character must be a letter.')
         return ''
-        
+
     return name
-    
-    
-    
+
+
 def _autorange_srs(srs, max_changes=1):
     """
-    hey fuck you joe -paul
+    Autoranges the SR lockins
     """
+
     def autorange_once():
         r = srs.R.get()
         sens = srs.sensitivity.get()
@@ -158,28 +174,24 @@ def _autorange_srs(srs, max_changes=1):
             decrement_sensitivity()
             return True
         return False
-    
+
     def increment_sensitivity():
         if srs.signal_input() == 'voltage':
             sense = srs._VOLT_TO_N[srs.sensitivity.get()]
-            srs.sensitivity.set(srs._N_TO_VOLT[int(sense-1)])
+            srs.sensitivity.set(srs._N_TO_VOLT[int(sense - 1)])
         else:
             sense = srs._CURR_TO_N[srs.sensitivity.get()]
-            srs.sensitivity.set(srs._N_TO_CURR[int(sense-1)])
-        
+            srs.sensitivity.set(srs._N_TO_CURR[int(sense - 1)])
+
     def decrement_sensitivity():
         if srs.signal_input() == 'voltage':
             sense = srs._VOLT_TO_N[srs.sensitivity.get()]
-            srs.sensitivity.set(srs._N_TO_VOLT[int(sense+1)])
+            srs.sensitivity.set(srs._N_TO_VOLT[int(sense + 1)])
         else:
             sense = srs._CURR_TO_N[srs.sensitivity.get()]
-            srs.sensitivity.set(srs._N_TO_CURR[int(sense+1)])
-        
-        
+            srs.sensitivity.set(srs._N_TO_CURR[int(sense + 1)])
+
     sets = 0
     while sets < max_changes and autorange_once():
         sets += 1
-        time.sleep(10*srs.time_constant.get())
-                 
-                 
-                 
+        time.sleep(10 * srs.time_constant.get())
