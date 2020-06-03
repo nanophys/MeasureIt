19b0b0a0c79e7a72322c53436d6fd01e453c6493
Elliott Runburg <erunburg@gmail.com>
2020-06-03 12:48:27 -0700

    Added dataset signal handling, safer detection of end of sweep.

--- src/runner_thread.py
+++ src/runner_thread.py
@@ -1,13 +1,17 @@
 # runner_thread.py
 
-from PyQt5.QtCore import QThread
+from PyQt5.QtCore import QThread, pyqtSignal
 import time
+
+from qcodes.dataset.data_set import DataSet
+
 
 class RunnerThread(QThread):
     """
     Class to separate to a new thread the communications with the instruments.
     """
-    
+    get_dataset = pyqtSignal(dict)
+
     def __init__(self, sweep):
         """
         Initializes the object by taking in the parent sweep object, initializing the 
@@ -23,17 +27,18 @@ class RunnerThread(QThread):
         self.db_set = False
         self.kill_flag = False
         self.flush_flag = False
-        
+        self.runner = None
+
         QThread.__init__(self)
-        
-        
+
     def __del__(self):
         """
         Standard destructor.
         """
+        # if self.runner is not None:
+        #    self.runner.__exit__()
         self.wait()
-    
-    
+
     def add_plotter(self, plotter):
         """
         Adds the PlotterThread object, so the Runner knows where to send the new
@@ -44,8 +49,7 @@ class RunnerThread(QThread):
                       sweep
         """
         self.plotter = plotter
-        
-        
+
     def _set_parent(self, sweep):
         """
         Function to tell the runner who the parent is, if created independently.
@@ -54,8 +58,7 @@ class RunnerThread(QThread):
             sweep - Object of type BaseSweep, that Runner will be taking data for
         """
         self.sweep = sweep
-        
-        
+
     def run(self):
         """
         Function that is called when new thread is created. NOTE: start() is called
@@ -63,31 +66,47 @@ class RunnerThread(QThread):
         Iterates the sweep, then hands the data to the plotter for live plotting.
         """
         # Check database status
-        if self.db_set == False and self.sweep.save_data == True:
-            self.datasaver = self.sweep.meas.run().__enter__()
-        
-        #print(f"called runner from thread: {QThread.currentThreadId()}")
+        if self.sweep.save_data is True:
+            self.runner = self.sweep.meas.run()
+            self.datasaver = self.runner.__enter__()
+            self.dataset = self.datasaver.dataset
+            ds_dict = {}
+            ds_dict['db'] = self.dataset.path_to_db
+            ds_dict['run id'] = self.dataset.run_id
+            ds_dict['exp name'] = self.dataset.exp_name
+            ds_dict['sample name'] = self.dataset.sample_name
+            self.get_dataset.emit(ds_dict)
+
+        # print(f"called runner from thread: {QThread.currentThreadId()}")
         # Check if we are still running
         while self.kill_flag is False:
             t = time.monotonic()
-            
+            # print(f'kill flag = {str(self.kill_flag)}, is_running={self.sweep.is_running}')
+
             if self.sweep.is_running is True:
                 # Get the new data
                 data = self.sweep.update_values()
-                
+                # Check if we've hit the end- update_values will return None
+                if data is None:
+                    continue
+
                 # Send it to the plotter if we are going
                 # Note: we check again if running, because we won't know if we are
                 # done until we try to step the parameter once more
                 if self.sweep.is_running is True and self.plotter is not None and self.sweep.plot_data is True:
                     self.plotter.add_data_to_queue(data, self.sweep.direction)
-                
+
             # Smart sleep, by checking if the whole process has taken longer than
             # our sleep time
             sleep_time = self.sweep.inter_delay - (time.monotonic() - t)
-            
+
             if sleep_time > 0:
                 time.sleep(sleep_time)
-            
-            if self.flush_flag == True and self.sweep.save_data is True:
+
+            if self.flush_flag is True and self.sweep.save_data is True:
                 self.datasaver.flush_data_to_database()
                 self.flush_flag = False
+            # print('at end of kill flag loop')
+
+        if self.runner is not None:
+            self.runner.__exit__()
