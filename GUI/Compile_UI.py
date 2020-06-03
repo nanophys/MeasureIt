e6813dd9205274cdf31edeeb687326d472525dbd
Elliott Runburg <erunburg@gmail.com>
2020-06-03 12:47:01 -0700

    Updated with new dialogs.

--- GUI/Compile_UI.py
+++ GUI/Compile_UI.py
@@ -1,7 +1,8 @@
 
 from PyQt5 import uic
 import os
-ui_files = ['mainwindow.ui', 'add_device.ui', 'edit_parameter.ui', 'save_station.ui', 'remove_device.ui', 'save_data.ui']
+ui_files = ['mainwindow.ui', 'add_device.ui', 'edit_parameter.ui', 'save_station.ui', 
+            'remove_device.ui', 'save_data.ui', 'edit_sweep.ui', 'view_dataset.ui']
 for ui_file in ui_files:
     py_file = os.path.splitext(ui_file)[0] + "_ui.py"
     fp = open(py_file, "w")
