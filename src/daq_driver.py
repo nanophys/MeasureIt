329e4681137a503da21a3d27ae43889c2d77d25a
Elliott Runburg <erunburg@gmail.com>
2021-01-13 14:28:49 -0800

    Changed reading of AO channels to read the machine, rather than a file

--- src/daq_driver.py
+++ src/daq_driver.py
@@ -41,8 +41,9 @@ class Daq(Instrument):
             channel = DaqAOChannel(self, self.device, self._address, ch_name)
             self.add_submodule(ch_name, channel)
             # We now automatically create a task to write on each channel (NECESSARY!)
-            task = nidaqmx.Task(f"writing {self._address}_{ch_name}")
-            channel.add_self_to_task(task)
+            write_task = nidaqmx.Task(f"writing {self._address}_{ch_name}")
+            read_task = nidaqmx.Task(f"reading {self._address}_{ch_name}")
+            channel.add_self_to_task(write_task, read_task)
         # Similarly, create a DaqAIChannel object for each (real) input channel
         for b in range(self.ai_num):
             # NI DAQs can use each of the coax pins as analog inputs separately, and
@@ -112,11 +113,11 @@ class Daq(Instrument):
         return snap
 
     def close(self):
+        for a, c in self.submodules.items():
+            c.close()
+
         with open(self.cfg_fp, 'w') as conf:
             self.cfg_obj.write(conf)
-
-        for a, c in self.submodules.items():
-            c.clear_task()
 
         super().close()
 
@@ -178,16 +179,17 @@ class DaqAOChannel(InstrumentChannel):
         """
         Returns the current output voltage.
         """
-        self._voltage = float(self.parent.cfg_output[self.my_name])
+        if self.read_task is not None:
+            self._voltage = self.read_task.read()
+
         return self._voltage
 
     def set_voltage(self, _voltage):
         """
         Sets the voltage to the value passed as '_voltage'
         """
-        if self.task is not None:
-            self.task.write(_voltage)
-            self.parent.cfg_output[self.my_name] = str(_voltage)
+        if self.write_task is not None:
+            self.write_task.write(_voltage)
 
             self._voltage = _voltage
 
@@ -245,7 +247,8 @@ class DaqAOChannel(InstrumentChannel):
         self.impedance = None
         self._value = 0
 
-        self.task = None
+        self.write_task = None
+        self.read_task = None
         self.channel = None
 
         # Add a Parameter for the output gain, set the unit to 'V/V' as default
@@ -288,22 +291,24 @@ class DaqAOChannel(InstrumentChannel):
                            vals=vals.Numbers(-10, 10)
                            )
 
-    def add_self_to_task(self, task):
+    def add_self_to_task(self, write_task, read_task):
         """
         Utility function to define the task that should be used to conduct writes
         to the port.
         Arguments:
             task - task to be used for writing
         """
-        if self.task is not None:
+        if self.write_task is not None or self.read_task is not None:
             self.clear_task()
 
         # Add the channel to the task
-        task.ao_channels.add_ao_voltage_chan(self.fullname)
-        self.task = task
+        write_task.ao_channels.add_ao_voltage_chan(self.fullname)
+        self.write_task = write_task
+        read_task.ai_channels.add_ai_voltage_chan(f"{self.address}/_{self.my_name}_vs_aognd")
+        self.read_task = read_task
         # Channel handler that can be used to communicate things like gain, impedance
         # back to the DAQ
-        self.channel_handle = nidaqmx._task_modules.channels.ao_channel.AOChannel(self.task._handle, self.channel)
+        self.channel_handle = nidaqmx._task_modules.channels.ao_channel.AOChannel(self.write_task._handle, self.channel)
 
     #        if self.gain != -1:
     #            task.ao_channels.ao_gain=self.gain
@@ -314,15 +319,19 @@ class DaqAOChannel(InstrumentChannel):
         """
         Utility function to clean the task object for resource maintenance reasons
         """
-        if self.task is not None:
-            self.task.close()
-        self.task = None
+        if self.read_task is not None:
+            self.read_task.close()
+        if self.write_task is not None:
+            self.write_task.close()
+        self.read_task = None
+        self.write_task = None
         self.channel_handle = None
 
-    def __del__(self):
+    def close(self):
         """
         Destructor, makes sure task is clean.
         """
+        self.parent.cfg_output[self.my_name] = str(self._voltage)
         self.clear_task()
 
 
@@ -362,7 +371,7 @@ class DaqAIChannel(InstrumentChannel):
         """
         Returns the current input voltage.
         """
-        if self.task != None:
+        if self.task is not None:
             self._voltage = self.task.read()
         return self._voltage
 
@@ -483,7 +492,7 @@ class DaqAIChannel(InstrumentChannel):
         self.task = None
         self.channel_handle = None
 
-    def __del__(self):
+    def close(self):
         """
         Destructor, makes sure task is clean.
         """
