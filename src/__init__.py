1756251af3806789e6850dbbfb45ba601c51d99c
Elliott Runburg <erunburg@gmail.com>
2021-08-18 18:49:50 -0700

    Added import statements for sweeps and util to prevent need to reference source file name for each object import

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,13 @@
 from os.path import dirname, basename, isfile, join
 import glob
+
+from .sweep0d import Sweep0D
+from .sweep1d import Sweep1D
+from .sweep2d import Sweep2D
+from .sweep_queue import SweepQueue, DatabaseEntry
+from .sweep_ips import SweepIPS
+from .simul_sweep import SimulSweep
+from . import util
+
 modules = glob.glob(join(dirname(__file__), "*.py"))
-__all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
+__all__ = [basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
