7361ab1f0c55ae7b74adfe530b2b739f8b7977e4
Jiaqi Cai <caidish1234@gmail.com>
2021-08-26 01:32:25 -0700

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -8,6 +8,8 @@ from .sweep_queue import SweepQueue, DatabaseEntry
 from .sweep_ips import SweepIPS
 from .simul_sweep import SimulSweep
 from . import util
+from .safe_ramp import safe_ramp
+from .gate_leakage import GateLeakage
 
 modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
