9b3a68bbe5feaf9d0efddd7bb6a0f47086799bea
bigblue <nanophys@uw.edu>
2023-12-01 10:25:35 -0800

    Added import statements into init so one can call 'from MeasureIt import SweepXX' directly

--- src/MeasureIt/__init__.py
+++ src/MeasureIt/__init__.py
@@ -2,6 +2,13 @@ from os.path import dirname, basename, isfile, join
 import glob
 from qcodes import config
 
+from .sweep0d import Sweep0D
+from .sweep1d import Sweep1D
+from .sweep2d import Sweep2D
+from .sweep_queue import SweepQueue
+from .simul_sweep import SimulSweep
+from .sweep_ips import SweepIPS
+
 config.logger.start_logging_on_import = "always"
 config.logger.console_level = "WARNING"
 config.station.enable_forced_reconnect = False
